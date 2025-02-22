93cc1062479058d784c29e0b99b85ef77fc4a008
akasimonmusic <a.k.a.simonmusic@gmail.com>
2025-02-22 01:13:55 +0100

    added subscription

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -1,5 +1,4 @@
----
----
+
 $(function() {
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
@@ -10,11 +9,12 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
+	    //var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
+	    var url = "https://formspree.io/f/mdkajngw";
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
+      //var phone = $("input#phone").val();
+      //var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
@@ -25,12 +25,10 @@ $(function() {
       $.ajax({
         url: url,
         type: "POST",
-	dataType: "json",
+	      dataType: "json",
         data: {
           name: name,
-          phone: phone,
-          email: email,
-          message: message
+          email: email
         },
         cache: false,
 
@@ -40,7 +38,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Your subscription has been sent. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
